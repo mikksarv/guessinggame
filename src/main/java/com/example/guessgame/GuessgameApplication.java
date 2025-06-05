e42dccf123108531aa02ce435105d64ed7321555
mikksarv <mikksrv@gmail.com>
2025-06-05 16:02:31 +0300

    Initial push

---
+++ src/main/java/com/example/guessgame/GuessgameApplication.java
@@ -0,0 +1,14 @@
+package com.example.guessgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessgameApplication.class, args);
+		System.out.println("Application started");
+	}
+
+}
